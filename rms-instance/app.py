1b4c1c893f3ebe86d84c891373303d2c28c0365e
tirth pipalia <56732761+tirth-pipalia@users.noreply.github.com>
2022-04-04 17:42:27 +0100

    Update app.py

--- rms-instance/app.py
+++ rms-instance/app.py
@@ -6,7 +6,7 @@ from rms_instance.rms_instance_stack import RmsInstanceStack
 
 
 app = cdk.App()
-env_ME = cdk.Environment(account="620228650709", region="me-south-1")
+env_ME = cdk.Environment(account="12_digit_account_no", region="me-south-1")
 RmsInstanceStack(app, "RmsInstanceStack", env=env_ME)
 
 app.synth()
